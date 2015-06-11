b7960960985fb5a651597145309cb21e065e6c07
butoibogdan <butoibogdan@yahoo.com>
2015-06-11 18:41:42 +0300

    Ok

--- config/app.php
+++ config/app.php
@@ -148,7 +148,7 @@ return [
 		'App\Providers\EventServiceProvider',
 		'App\Providers\RouteServiceProvider',
                 
-                'Customappzcoder\CrudGenerator\CrudGeneratorServiceProvider',
+                'Butoibogdan\CrudGenerator\CrudGeneratorServiceProvider',
                 'Intervention\Image\ImageServiceProvider',
                 'DaveJamesMiller\Breadcrumbs\ServiceProvider',
                 
