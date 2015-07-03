5cff1c32fedc4b37e600c1403fe821dddcd6c054
butoibogdan <butoibogdan@yahoo.com>
2015-07-03 15:08:27 +0300

    Ok

--- resources/views/administrare/invoicetpl.blade.php
+++ resources/views/administrare/invoicetpl.blade.php
@@ -1,33 +1,34 @@
 
 
-<div class="container">
-    <div class="row">
 
-        <h1>Produse Factura</h1>
-        <div class="input_fields_wrap">
-                <div class="row page-header">
-                    <div class="col-md-1"><h4>Nr.Crt.</h4></div>
-                    <div class="col-md-7"><h4>Denumire Produs</h4></div>
-                    <div class="col-md-1"><h4>Cant.</h4></div>
-                    <div class="col-md-1"><h4>Pret</h4></div>
-                    <div class="col-md-1"><h4>TVA</h4></div>
+<h2>Produse Factura</h2>
+
+<div class="col-md-12">
+    <div class="box box-primary">
+        <div class="box-header with-border">  
+                <div class="col-md-1"><h3 class="box-title">Nr.Crt.</h3></div>
+                <div class="col-md-7"><h3 class="box-title">Denumire Produs</h4></div>
+                <div class="col-md-1"><h3 class="box-title">Cant.</h3></div>
+                <div class="col-md-1"><h3 class="box-title">Pret</h3></div>
+                <div class="col-md-1"><h3 class="box-title">TVA</h3></div>
+                <div class="col-md-1"><h3 class="box-title"></h3></div>
+        </div>
+        <div class="box-body">
+            <div class="input_fields_wrap">
+                <div style="padding-bottom: 5px;" class="row">
+                    <div class="col-md-1 "><div class="form-control">1</div></div>
+                    <div class="col-md-7"><input class="form-control" type="text" name="denprodus[]"></div>
+                    <div class="col-md-1"><input class="form-control" type="text" name="cantitate[]"></div>
+                    <div class="col-md-1"><input class="form-control" type="text" name="pret[]"></div>
+                    <div class="col-md-1"><input class="form-control" type="text" name="tva[]"></div>
                     <div class="col-md-1"></div>
                 </div>
-            
-            <div style="padding-bottom: 5px;" class="row">
-                <div class="col-md-1 "><div class="form-control">1</div></div>
-                <div class="col-md-7"><input class="form-control" type="text" name="denprodus[]"></div>
-                <div class="col-md-1"><input class="form-control" type="text" name="cantitate[]"></div>
-                <div class="col-md-1"><input class="form-control" type="text" name="pret[]"></div>
-                <div class="col-md-1"><input class="form-control" type="text" name="tva[]"></div>
-                <div class="col-md-1"></div>
             </div>
         </div>
-        <br/>
-        <button class="add_field_button btn btn-info btn-sm"><i class="fa fa-plus"></i></button>
     </div>
 </div>
-<br/> 
+<button class="add_field_button btn btn-info btn-sm"><i class="fa fa-plus"></i></button>
+
 
 
 
--- resources/views/administrare/pages/facturi/create.blade.php
+++ resources/views/administrare/pages/facturi/create.blade.php
@@ -56,6 +56,7 @@
                         </div>
                     </div>
                     @include('administrare.invoicetpl')
+                    <br/><br/>
                     <div class="form-group">
                         {!! Form::submit('Adauga', ['class' => 'btn btn-primary form-control']) !!}
                     </div>
