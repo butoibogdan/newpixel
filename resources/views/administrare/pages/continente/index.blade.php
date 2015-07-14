6b1ae0418082db0e93fe97861d3c481e22f373b3
Catalin Prodan <catalin.prodan@newpixel.ro>
2015-07-14 13:02:33 +0300

    Aranjat sectiunea de continente
    Aranjat sectiunea de continente

--- resources/views/administrare/pages/continente/index.blade.php
+++ resources/views/administrare/pages/continente/index.blade.php
@@ -2,24 +2,33 @@
 
 @section('continente')
     <div class="content">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Dashboard</div>
-                    
-                    <div class="panel-body">
-                        <h1>Continente</h1>
-                        <h2><a href="{{ url('admin/continente/create') }}">Create</a></h2>
+
+        <div class="box box-default color-palette-box">
+            <div class="box-header with-border"> <h3 class="box-title"><i class="fa fa-th"></i> Mapamond :: continente</h3> </div>
+
+            <div class="box-body">
+                <div class="row">
+                    <div class="col-md-12">
+                        <a href="{{ url('admin/continente/create') }}" class="btn btn-app" title="Adauga"> <i class="fa fa-edit"></i> Adauga </a>
                         <div class="table-responsive">
                             <table class="table">
                                 <tr>
-                                    <th>SL.</th><th>Denumire</th><th>Actions</th>
+                                    <th class="col-sm-1">Nr.</th>
+                                    <th class="col-sm-9">Denumire</th>
+                                    <th class="col-sm-2 text-right">Actiuni</th>
                                 </tr>
                                 {{-- */$x=0;/* --}}
                                 @foreach($continentes as $item)
                                     {{-- */$x++;/* --}}
                                     <tr>
-                                        <td>{{ $x }}</td><td><a href="{{ url('admin/continente', $item->id) }}">{{ $item->Denumire }}</a></td><td><a href="{{ url('admin/continente/edit/'.$item->id) }}">Edit</a> / <a href="{{ url('admin/continente/destroy/'.$item->id) }}">Delete</a> </td>
+                                        <td>{{ $x }}</td>
+                                        <td>
+                                            <a href="{{ url('admin/continente', $item->id) }}">{{ $item->Denumire }}</a>
+                                        </td>
+                                        <td class="text-right">
+                                            <a href="{{ url('admin/continente/edit/'.$item->id) }}" class="btn btn-primary btn-xs" title="Modifica">Modifica</a>
+                                            <a href="{{ url('admin/continente/destroy/'.$item->id) }}" class="btn btn-danger btn-xs" title="Modifica">Sterge</a>
+                                        </td>
                                     </tr>
                                 @endforeach
                             </table>
