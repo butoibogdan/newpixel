e8c4ef12bc5ebc19bb1d3fcad58cc95e6c8a3d7f
Catalin Prodan <catalin.prodan@newpixel.ro>
2015-07-14 14:19:33 +0300

    Modificat tari si continente
    Modificat tari si continente

--- resources/views/administrare/pages/tari/show.blade.php
+++ resources/views/administrare/pages/tari/show.blade.php
@@ -1,25 +1,32 @@
 @extends('administrare.dashboard_body')
 
 @section('tari')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-10 col-md-offset-1">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Dashboard</div>
+    <div class="content">
 
-                    <div class="panel-body">
-                        <h1>tari</h1>
+        <div class="box box-default color-palette-box">
+            <div class="box-header with-border"> <h3 class="box-title"><i class="fa fa-th"></i> Mapamond :: tari :: {{$tari->nume}}</h3> </div>
+
+            <div class="box-body">
+                <div class="row">
+                    <div class="col-md-12">
+                        <a href="{{ url('admin/tari/create') }}" class="btn btn-app" title="Adauga"> <i class="fa fa-edit"></i> Adauga </a>
                         <div class="table-responsive">
                             <table class="table">
                                 <tr>
-                                    <th>ID.</th><th>Name</th>
+                                    <th class="col-sm-1">Nr.</th>
+                                    <th class="col-sm-11">Denumire</th>
                                 </tr>
                                 <tr>
-                                    <td>{{ $tari->id }}</td><td>{{ $tari->name }}</td>
+                                    <td>{{ $tari->id }}</td><td>{{ $tari->nume }}</td>
                                 </tr>
                             </table>
                         </div>
                     </div>
+                </div>
+            </div>
+            <div class="box-footer">
+                <div class="form-group">
+                    <div class="col-md-2 col-xs-offset-10"><a href = "{{URL::previous()}}" class = 'btn btn-block btn-default btn-sm'>Inapoi la lista</a></div>
                 </div>
             </div>
         </div>
