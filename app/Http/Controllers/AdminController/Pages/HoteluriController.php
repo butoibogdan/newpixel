60e2f2f913fa799db16eff80e55917dcac2d51b2
butoibogdan <butoibogdan@yahoo.com>
2015-07-19 12:46:44 +0300

    Ok

--- app/Http/Controllers/AdminController/Pages/HoteluriController.php
+++ app/Http/Controllers/AdminController/Pages/HoteluriController.php
@@ -165,7 +165,7 @@ class HoteluriController extends Controller {
                 $fileName = rand(11111, 99999) . '.' . $extension; // renameing image
                 $upload = $file->move($destinationPath, $fileName);
                 $url=\URL::asset('images') . "/" . $fileName;
-                Image::make($url)->resize(\Config::get('newpixel.width'), \Config::get('newpixel.height'))->save('images/' . $fileName);
+                Image::make($url)->resize('200','200')->save('images/' . $fileName);
 
                 $valoripoze = array(
                     'HotelID' => $id,
